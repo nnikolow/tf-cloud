7ccdf39d8833bf78119af277cc43d65d7a0b6c2d
nnikolow <98734722+nnikolow@users.noreply.github.com>
2022-05-10 15:10:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,26 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.16.0"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "nn-org1"
+
+    workspaces {
+      name = "gh-actions-demo"
     }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {
